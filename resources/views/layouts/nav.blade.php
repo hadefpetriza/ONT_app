a63ef9b20dddfca749100e00d3c5e75312f6a611
Latifatuzikra Suhairi <latifatuzikrasuhairi@users.noreply.github.com>
2022-05-31 21:15:13 +0700

    first UI

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+		<div class="container">
+			<a class="navbar-brand fw-bold" href="#">
+                <img src="{{ asset('images/logo.png') }}" alt="logo" height="24" class="d-inline-block align-text-top">
+                PT TELEKOMUNIKASI INDONESIA
+            </a> 
+                <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse" type="button">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Optical Network Terminal (ONT)</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Logout</a>
+                    </li>
+				</ul>
+			</div>
+		</div>
+	</nav>
